7e1bb7e82420473f43b4586966a1d69ad82a5a6a
nickbanana <nick.chi@gmail.com>
2017-07-07 13:33:54 +0800

    add dictionary

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from nlp import SnowNLP
 
 
 def main():
+    list1 = []
+    list2 = []
+    dup = True
+    path = 'userdict.txt'
+    dict_file = open(path,'r')
+    list1 = dict_file.readlines()
+    for i in list1:
+        list2.append(i.rstrip('\n'))
     sentence = input("請輸入句子:")
     nlpObj = SnowNLP(sentence)
-    keyword = nlpObj.keywords(4)
-    print(keyword)
+    for i in list2:
+        if i in sentence and dup is True:
+            print("想知道" + i + "的內容")
+            dup = False
 
 if __name__ == '__main__':
     main()
