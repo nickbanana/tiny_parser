b79cbcd59127f65a0dd9f9b313c5a1bae5258d7a
nickbanana <nick.chi@gmail.com>
2017-07-07 13:43:24 +0800

    fix something

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     list1 = []
     list2 = []
     dup = True
+    Find = False
     path = 'userdict.txt'
     dict_file = open(path,'r')
     list1 = dict_file.readlines()
@@ -12,10 +13,18 @@ def main():
         list2.append(i.rstrip('\n'))
     sentence = input("請輸入句子:")
     nlpObj = SnowNLP(sentence)
-    for i in list2:
-        if i in sentence and dup is True:
-            print("想知道" + i + "的內容")
-            dup = False
+    keywords = nlpObj.keywords(3)
+    for i in keywords:
+        for j in list2:
+            if i == j:
+                print("想知道" + i + "的內容")
+                Find = True
+
+    if Find is False:
+        for i in list2:
+            if i in sentence and dup is True:
+                print("想知道" + i + "的內容")
+                dup = False
 
 if __name__ == '__main__':
     main()
